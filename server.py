c88d37b9f2422f845269617400ec2518fea6aa21
Augustokark <augustokark@hotmail.com>
2024-03-21 13:30:37 -0300

    agregamos persistencia

--- server.py
+++ server.py
@@ -1,23 +1,23 @@
-
 import socketserver
 import select
 import socket
-import threading
-
+import os
 
 MAX_CLIENTS = 30
 PORT = 22223
 QUIT_STRING = '<$quit$>'
+HISTORY_FILE = 'chat_history.txt'
 
 
-# def create_socket(address):
-#     s = socket.socket(socket.AF_UNSPEC, socket.SOCK_STREAM)
-#     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#     s.setblocking(0)
-#     s.bind(address)
-#     s.listen(MAX_CLIENTS)
-#     print("Servidor escuchando en ", address) 
-#     return s
+def create_socket(address):
+    s = socket.socket(socket.AF_UNSPEC, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.setblocking(0)
+    s.bind(address)
+    s.listen(MAX_CLIENTS)
+    print("Servidor escuchando en ", address)
+    return s
+
 
 class Hall:
     def __init__(self):
@@ -31,7 +31,7 @@ class Hall:
     def list_rooms(self, player):
         if len(self.rooms) == 0:
             msg = 'Lo sentimos pero no hay salas creadas hasta el momento. Crea una!\n' \
-                + 'Usa [<join> room_name] to create a room.\n'
+                  + 'Usa [<join> room_name] to create a room.\n'
             player.socket.sendall(msg.encode())
         else:
             msg = 'Listando salas actuales...\n'
@@ -40,13 +40,13 @@ class Hall:
             player.socket.sendall(msg.encode())
 
     def handle_msg(self, player, msg):
-        instructions = b'Instrucciones:\n'\
-            + b'[<list>] para listar todas las salas\n'\
-            + b'[<join> room_name] para unirte/crear/cambiar a una sala\n' \
-            + b'[<manual>] para mostrar las instrucciones\n' \
-            + b'[<quit>] para salir\n' \
+        instructions = b'Instrucciones:\n' \
+                        + b'[<list>] para listar todas las salas\n' \
+                        + b'[<join> room_name] para unirte/crear/cambiar a una sala\n' \
+                        + b'[<history>] para ver el historial de chat\n' \
+                        + b'[<manual>] para mostrar las instrucciones\n' \
+                        + b'[<quit>] para salir\n'
 
-            
         print(player.name + " dice: " + msg)
         if "name:" in msg:
             name = msg.split()[1]
@@ -85,13 +85,17 @@ class Hall:
             player.socket.sendall(QUIT_STRING.encode())
             self.remove_player(player)
 
+        elif "<history>" in msg:
+            self.show_history(player)
+
         else:
             if player.name in self.room_player_map:
                 self.rooms[self.room_player_map[player.name]].broadcast(player, msg.encode())
+                self.save_to_history(player.name, msg)  # Save message to history file
             else:
                 msg = 'Actualmente no estás en ninguna sala.\n' \
-                    + 'Usa [<list>] para ver las salas disponibles.\n' \
-                    + 'Usa [<join> room_name] para unirte a una sala.\n'
+                      + 'Usa [<list>] para ver las salas disponibles.\n' \
+                      + 'Usa [<join> room_name] para unirte a una sala.\n'
                 player.socket.sendall(msg.encode())
 
     def remove_player(self, player):
@@ -99,6 +103,20 @@ class Hall:
             self.rooms[self.room_player_map[player.name]].remove_player(player)
             del self.room_player_map[player.name]
         print("Jugador: " + player.name + " ha abandonado el chat\n")
+
+    def show_history(self, player):
+        if os.path.exists(HISTORY_FILE):
+            with open(HISTORY_FILE, 'r') as file:
+                history = file.read()
+                player.socket.sendall(history.encode())
+        else:
+            player.socket.sendall(b'No hay historial de chat disponible.\n')
+
+    @staticmethod
+    def save_to_history(player_name, message):
+        with open(HISTORY_FILE, 'a') as file:
+            file.write(player_name + ": " + message + "\n")
+
 
 class Room:
     def __init__(self, name):
@@ -120,6 +138,7 @@ class Room:
         leave_msg = player.name.encode() + b"ha abandonado la sala\n"
         self.broadcast(player, leave_msg)
 
+
 class Player:
     def __init__(self, socket, name="nuevo"):
         socket.setblocking(0)
@@ -130,27 +149,6 @@ class Player:
         return self.socket.fileno()
 
 
-
-# class ChatHandler(socketserver.StreamRequestHandler):
-#     def handle(self):
-#         player = Player(self.request)
-#         hall.welcome_new(player)
-#         while True:
-#             read_sockets, _, _ = select.select([player.socket], [], [])
-#             for sock in read_sockets:
-#                 msg = sock.recv(4096)
-#                 if not msg:
-#                     player.socket.close()
-#                     return
-#                 msg = msg.decode().lower()
-#                 hall.handle_msg(player, msg)
-
-
-# hall = Hall()
-# server = socketserver.ThreadingTCPServer(('localhost', PORT), ChatHandler)
-# server.allow_reuse_address = True
-# print("Servidor escuchando en el puerto", PORT)
-# server.serve_forever()
 class ChatHandler(socketserver.StreamRequestHandler):
     def handle(self):
         player = Player(self.request)
@@ -165,25 +163,9 @@ class ChatHandler(socketserver.StreamRequestHandler):
                 msg = msg.decode().lower()
                 hall.handle_msg(player, msg)
 
+
 hall = Hall()
 server = socketserver.ThreadingTCPServer(('localhost', PORT), ChatHandler)
 server.allow_reuse_address = True
 print("Servidor escuchando en el puerto", PORT)
-
-# Función para iniciar el servidor en un hilo aparte
-def start_server(server):
-    server.serve_forever()
-
-# Iniciar el servidor en un hilo aparte
-server_thread = threading.Thread(target=start_server, args=(server,))
-server_thread.daemon = True
-server_thread.start()
-
-# Esperar hasta que se presione Ctrl+C para detener el servidor
-try:
-    while True:
-        pass
-except KeyboardInterrupt:
-    server.shutdown()
-    server.server_close()
-    print("Servidor detenido")
+server.serve_forever()
